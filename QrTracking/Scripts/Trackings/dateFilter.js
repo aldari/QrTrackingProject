12f8d7d91fc414eeffc4eaa793dca4933d81be41
El'dar Ismagilov <eldarismagilov@gmail.com>
2016-02-02 15:25:02 +0500

    Adding date filter

---
+++ QrTracking/Scripts/Trackings/dateFilter.js
@@ -0,0 +1,17 @@
+﻿'use strict';
+
+registrationModule.filter('upComing', function () {
+    return function (items, field, day1, day2) {
+        if (day1 == null)
+            day1 = new Date('01/01/1970');
+        if (day2 == null)
+            day2 = new Date('01/01/3000');
+        var timeStart = new Date(day1);
+        var timeEnd = new Date(day2);
+
+        return items.filter(function (item) {
+            var d = new Date(item[field]);
+            return (d > timeStart && d < timeEnd);
+        });
+    };
+});
--- QrTracking/Scripts/Trackings/trackings-controller.js
+++ QrTracking/Scripts/Trackings/trackings-controller.js
@@ -6,4 +6,20 @@ registrationModule.controller("TrackingsController", function($scope, trackingRe
     $scope.addTracking = function() {
         $location.path('/Trackings/Add');
     };
+    
+    $scope.popup1 = {
+        opened: false
+    };
+
+    $scope.open1 = function () {
+        $scope.popup1.opened = true;
+    };
+    
+    $scope.popup2 = {
+        opened: false
+    };
+
+    $scope.open2 = function () {
+        $scope.popup2.opened = true;
+    };
 });
